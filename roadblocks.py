5f6a43c33ca9620297f65e56522bce5dc51cf0f5
Reilly <reilly.ford@gmail.com>
2021-02-06 10:08:27 -0500

    initial commit after i already did most of it lol

--- roadblocks.py
+++ roadblocks.py
@@ -11,16 +11,19 @@ def main():
     travel_and_place_beeper()
 
 
-
-
-
 def travel_and_place_beeper():
     """Instructs Karel to run a full lap around an 8x7 track. Runs on world lab01_1.kwld.
 
-    pre-condition: Assumes Karel starts at position (1, 1), facing north
-    post-condition: Karel is facing north, and ending in the same position as the pre-condition
+    pre-condition: Assumes Karel starts at position (1, 1), facing east
+    post-condition: Karel is facing west, and ending in the same position as the pre-condition. There is now a beeper on the opposite end of the corridor he is now in (the beeper is at either (11, 1) or (8, 1) depending on which world you run it in)
     """
-    pass
+    while front_is_clear():  # makes karel move forward until he hits a wall
+        move()
+    put_beeper()  # places beeper next to wall
+    turn_left()
+    turn_left()  # makes a 180 degree turn
+    while front_is_clear():  # returns to other side of corridor
+        move()
 
 
 ####### DO NOT EDIT CODE BELOW THIS LINE ########
